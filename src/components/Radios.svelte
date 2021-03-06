bed530179dcd225e9e94e328f8d9b16ccb163aa7
erbridge <f@erbridge.co.uk>
2021-03-06 04:30:11 +0200

    Add a Radios component

---
+++ src/components/Radios.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let name: string;
+  export let items: { text: string; value: string }[];
+  export let initialValue: string | null = null;
+</script>
+
+<div class="govuk-radios">
+  {#each items as { text, value }}
+    <div class="govuk-radios__item">
+      <input
+        class="govuk-radios__input"
+        id="{name}-{value}"
+        {name}
+        type="radio"
+        {value}
+        checked={initialValue === value}
+      />
+      <label class="govuk-label govuk-radios__label" for="{name}-{value}">
+        {text}
+      </label>
+    </div>
+  {/each}
+</div>
